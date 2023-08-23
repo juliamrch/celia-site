73354c4c942a0bf94d29dc995d93dd70f3dba795
julia <julia.navarro@clever-cloud.com>
2023-08-23 23:28:23 +0200

    solve conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+
 export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8080
   },
   integrations: [mdx()]
-  
+
 });
